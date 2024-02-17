8e30498909a1d6812475764a489b4b2f4c0ad59c
BiggestChris <stylianouchristopher8@gmail.com>
2024-02-17 07:21:26 +0000

    Updating layout

--- app.py
+++ app.py
@@ -22,16 +22,6 @@ def contact():
     return render_template("contact.html")
 
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello_there(name = None):
-    return render_template(
-        "hello_there.html",
-        name=name,
-        date=datetime.now()
-    )
-
-
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
