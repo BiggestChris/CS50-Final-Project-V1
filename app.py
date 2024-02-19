826e7ec6d9abb5ce3fe1f09ee2e37db944a1e203
BiggestChris <stylianouchristopher8@gmail.com>
2024-02-19 20:25:50 +0000

    CSV Reader working

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 db = SQL("sqlite:///fitness.db")
 
 # Import workout after defining db and app
-from workout import exercise
+from workout import exercise, weight_import
 
 
 
@@ -38,9 +38,14 @@ def food():
     return render_template("food.html")
 
 
-@app.route("/weight")
+@app.route("/weight", methods=('GET', 'POST'))
 def weight():
-    return render_template("weight.html")
+    if request.method == 'POST':
+        weight_import()
+
+        return redirect("/")
+    else:
+        return render_template("weight.html")
 
 
 @app.route("/api/data")
--- workout.py
+++ workout.py
@@ -1,7 +1,9 @@
 import re, os
 from datetime import datetime
+import pandas as pd
 
 from flask import request
+from sqlalchemy import create_engine
 
 from app import db
     
@@ -35,3 +37,21 @@ def exercise():
                 db.execute("UPDATE exercise SET ? = ?, ? = ? WHERE ID = ?", f"Set{numbers[i].capitalize()}_Reps", sets[i], f"Set{numbers[i].capitalize()}_Weight", weights[i], last_inserted_id)
         except IndexError:
             pass
+
+    return "success"
+
+# Help received from ChatGPT refining this function
+def weight_import():
+    # Look for CSV file
+    data = r"C:\Users\styli\OneDrive\Documents\Coding\CS50\CS50 - Final Project - Git\Renpho Data\Renpho-Christopher 2024-02-19.csv"
+
+
+    # Read CSV file
+    df = pd.read_csv(data)
+
+    # Transpose CSV file into SQL
+    engine = create_engine("sqlite:///fitness.db")
+    df.to_sql('weight-1', engine, if_exists='replace', index=False)
+    engine.dispose()
+
+    return "success"
