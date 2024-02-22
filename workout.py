5992476bc7892a00b359ace003958f3aa43782b1
BiggestChris <stylianouchristopher8@gmail.com>
2024-02-22 21:08:52 +0000

    Export of Weight data works

--- workout.py
+++ workout.py
@@ -117,29 +117,81 @@ def export():
         else:
             print("Failed to create CSV file.")
 
-        # G-sheets authorisation
-        # TODO: Need to load in JSON securely - having it in Repo and then uploading publicly created a security risk
-            # json is now moved separately, but file location still in code which is a risk, repo moved to private for now until resolved
-        gc = pygsheets.authorize(service_file=r'C:\Users\styli\OneDrive\Documents\Coding\CS50\CS50 - Final Project - Git\GSheets API\g-sheets-for-python-a3ee6cd4d658.json')
+    # G-sheets authorisation
+    # TODO: Need to load in JSON securely - having it in Repo and then uploading publicly created a security risk
+        # json is now moved separately, but file location still in code which is a risk, repo moved to private for now until resolved
+    gc = pygsheets.authorize(service_file=r'C:\Users\styli\OneDrive\Documents\Coding\CS50\CS50 - Final Project - Git\GSheets API\g-sheets-for-python-a3ee6cd4d658.json')
 
-        # Create empty dataframe
-        df2 = pd.DataFrame()
+    # Create empty dataframe
+    df2 = pd.DataFrame()
 
-        # Create a column
-        df2['name'] = ['John', 'Steve', 'Sarah']
+    # Open the google spreadsheet (this has the key from the Greg Burns Fitness Sheet)
+    sh = gc.open_by_key('1F_6EtWT68BO2EfY_dErs-fNKWiEMCj497Hs0MnI-HsY')
 
-        #open the google spreadsheet (where 'PY to Gsheet Test' is the name of my sheet)
-        sh = gc.open_by_key('1F_6EtWT68BO2EfY_dErs-fNKWiEMCj497Hs0MnI-HsY')
+    #select the Daily Tracker worksheet 
+    wks = sh.worksheet_by_title('Daily Tracker')
 
-        #select the first sheet 
-        wks = sh.worksheet_by_title('Daily Tracker')
+    # TODO: Daily Tracker
+        # Read Column C to pick up dates
+    date_column = wks.get_col(3)
+    # Above creates a list of values in the cells in that column
 
-        # TODO: Daily Tracker
-            # Read Column C to pick up dates
-            # Need to compare to dates in Weight table
-            # Where they are matching - need to load in the weight into Column D
-            # If multiple entries for one day - pick the earliest time
+    # Need to compare to dates in Weight table
+    dates = db.execute('SELECT [Time of Measurement], [Weight(kg)] FROM weight')
+    # Above creates a list of dictionaries, with 'Time of Measurement' and 'Weight(kg)' as the keys in those dictionaries
 
-        # Update the first sheet with df, starting at cell - note (y,x) is format of set_dataframe coordinates.
-        # So wks.set_dataframe(df2,(9,4)) will update cell Row 9, Column D (4))
-        wks.set_dataframe(df2,(9,4))
+    fresh_dates = []
+
+    for x in dates:
+        date_object = datetime.strptime((x['Time of Measurement']), "%m/%d/%Y, %H:%M:%S")
+        formatted_date = date_object.strftime("%d/%m/%y")
+        fresh_dates.append({'date': formatted_date, 'weight': x['Weight(kg)']})
+
+    # Where they are matching - need to load in the weight into Column D
+    for index, date in enumerate(date_column):
+        # Ensure we skip the header row
+        if index == 0:
+            continue
+
+        # Define the regex patterns
+        pattern1 = r'^.{4}(\d{1}/\d{1}/\d{2})$'
+        pattern2 = r'^.{4}(\d{1}/\d{2}/\d{2})$'
+        pattern3 = r'^.{4}(\d{2}/\d{1}/\d{2})$'
+        pattern4 = r'^.{4}(\d{2}/\d{2}/\d{2})$'
+
+        match = re.match(pattern1, date) or re.match(pattern2, date) or re.match(pattern3, date) or re.match(pattern4, date)
+        if match:
+            # If the string matches the pattern
+            # TODO: Fix error in how this is working - currently carrying over whole string
+            extracted_part = match.group(1)
+            # Extract the part representing the month
+            day_part = extracted_part.split('/')[0]
+            month_part = extracted_part.split('/')[1]
+            # Check if the day or month part has only one digit
+            if len(day_part) == 1:
+                # Insert '0' before the single digit day
+                extracted_part = '0' + extracted_part
+            if len(month_part) == 1:
+                # Insert '0' before the single digit month
+                extracted_part = extracted_part[:3] + '0' + extracted_part[3:]
+
+            # Iterate over fresh_dates to find a matching date
+            for fresh_date in fresh_dates:              
+                if extracted_part == fresh_date['date']:
+                    # Update the weight in Column D
+                    wks.update_value(f"D{index+1}", fresh_date['weight'])
+                    break  # Break the loop once we find a match
+
+        else:
+            pass
+
+
+    wks.update_value(f"D6", 'TEST')   
+    # If multiple entries for one day - pick the earliest time
+
+
+
+
+    # Update the first sheet with df, starting at cell - note (y,x) is format of set_dataframe coordinates.
+    # So wks.set_dataframe(df2,(9,4)) will update cell Row 9, Column D (4))
+    wks.set_dataframe(df2,(9,4))
